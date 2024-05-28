22fa4ee2bf2ef29589fbffc3382f6fdefc682b06
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-05-29 02:13:02 +0530

    loop example

---
+++ src/Tut5.scala
@@ -0,0 +1,17 @@
+object Tut5 {
+  def main(args: Array[String]): Unit = {
+
+    // loops - scala support while/do-while and for loop
+
+    var x = 0
+    while (x < 10) {
+      println(s"val of x is ${x}")
+      x += 1
+    }
+
+
+
+
+
+  }
+}
