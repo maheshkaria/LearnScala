1a2fac8fae0049add443c2d5ec94201045d96189
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-05-29 01:56:20 +0530

    var type assignment

---
+++ src/Tut2.scala
@@ -0,0 +1,14 @@
+object Tut2 {
+  def main(args: Array[String]): Unit = {
+
+    // variable relationship with type of data
+
+    var z: Int = 10 // we can exclusively define what kind of value a variable holds
+    // type annotation helps to assign appropriate value
+    // usually type is inferred but its good to define
+
+    z = 123
+
+
+  }
+}
