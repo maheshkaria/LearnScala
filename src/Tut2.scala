78948124f1f4ae5c4995e68ad64a581a597d6aaa
maheshkaria <33889399+maheshkaria@users.noreply.github.com>
2024-05-29 01:58:27 +0530

    var type assignment

--- src/Tut2.scala
+++ src/Tut2.scala
@@ -6,9 +6,10 @@ object Tut2 {
     var z: Int = 10 // we can exclusively define what kind of value a variable holds
     // type annotation helps to assign appropriate value
     // usually type is inferred but its good to define
-
     z = 123
 
-
+    // this is how we can define a method
+    def add(x: Int, y: Int): Int = x + y
+    println(add(4,5))
   }
 }
